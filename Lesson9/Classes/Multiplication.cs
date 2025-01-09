79f1824ee6df2c73647f54f73cfc8ed2a23b27be
Emil Aliyev <aliyev.emil2209@gmail.com>
2025-01-09 22:12:38 +0400

    Lesson 11

--- Lesson9/Classes/Multiplication.cs
+++ Lesson9/Classes/Multiplication.cs
@@ -2,9 +2,12 @@ using Lesson9.Interfaces.ICalculatorOperation;
 
 namespace Lesson9.Classes.Multiplication;
 
-public class Multiplication : ICalculatorOperation{
+public class Multiplication : ICalculatorOperation
+{
     public string Name => "Multiplication";
-    public double Execute(double a, double b){
+
+    public double Execute(double a, double b)
+    {
         return a * b;
     }
 }
